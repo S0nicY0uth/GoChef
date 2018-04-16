e256003ff853e7216443a86d608516893e7cfbf0
chris_hopkins_2445 <cjhopkins1986@gmail.com>
2018-04-16 15:44:46 +0100

    added three users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+    {email: 'gordonramsey@gmail.com',password: 'pass123',name: 'Gordon Ramsey',location_lat: '51.512640',location_lon:'-0.090390'},
+    {email: 'deliasmith@gmail.com',password: 'pass123',name: 'Delia Smith',location_lat: '51.512427',location_lon:'-0.164376'},
+    {email: 'ainsleyharriet@gmail.com',password: 'pass123',name: 'Ainsley Harriet',location_lat: '51.485949',location_lon:'-0.106183'}
+]
+
+
+User.create!(users);
+
