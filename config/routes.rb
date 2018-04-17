3c3a91d5ac182ed128e465ca4b39a89cc8659626
Isobel Oakley <izzyoakley14@live.com>
2018-04-17 11:13:20 +0100

    renders views in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   
-  #root to: 'users#index'
+  root to: 'users#index'
 
-  get '/', to: 'application#index'
+  #get '/', to: 'application#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
