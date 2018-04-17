d275f52ef50ba2bfa049a0699036181a26a0316d
Kerri Marusiak <33982042+Kerrimaru@users.noreply.github.com>
2018-04-17 14:18:53 +0100

    added edit profile page buttony bits, added validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,33 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # def show
-  #   @users = User.all
-  # end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    if current_user.id == params[:id].to_i
+      @user = User.find(params[:id])
+    else
+      redirect_to '/'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render 'show'
+    else
+      render 'edit'
+    end    
+   
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :bio, :location_lat, :location_lon)
+  end
+
   
 end
