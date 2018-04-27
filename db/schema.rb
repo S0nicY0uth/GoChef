f2d7446df50a2351d66ba062296c787e7387c7ef
chris_hopkins_2445 <cjhopkins1986@gmail.com>
2018-04-27 13:15:46 +0100

    added banner upload functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426155804) do
-
+ActiveRecord::Schema.define(version: 20180427120905) do
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,10 @@ ActiveRecord::Schema.define(version: 20180426155804) do
     t.string "address"
     t.integer "role"
     t.integer "distance_to_travel"
+    t.string "banner_file_name"
+    t.string "banner_content_type"
+    t.integer "banner_file_size"
+    t.datetime "banner_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
